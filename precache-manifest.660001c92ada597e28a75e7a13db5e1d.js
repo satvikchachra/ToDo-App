05a64a40f7ab42fd34bcfd3f0ffc59c5567a8241
Satvik Chachra <50296407+satvikchachra@users.noreply.github.com>
2020-05-02 21:28:27 +0530

    Updates

---
+++ precache-manifest.660001c92ada597e28a75e7a13db5e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296c69b0fff047c350f4492042bfb1e8",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "611ecb636436db876179",
+    "url": "/ToDo-App/static/css/main.2674f854.chunk.css"
+  },
+  {
+    "revision": "d9c77c46f3dbd659dc81",
+    "url": "/ToDo-App/static/js/2.def871f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-App/static/js/2.def871f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "611ecb636436db876179",
+    "url": "/ToDo-App/static/js/main.54fd377b.chunk.js"
+  },
+  {
+    "revision": "1ad1e62184317a0f4571",
+    "url": "/ToDo-App/static/js/runtime-main.7e8c4adf.js"
+  }
+]);
